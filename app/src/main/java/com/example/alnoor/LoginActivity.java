b1b826a1705efee3960eac7cf20750e3323d0885
Roy <devedroy.dr@gmail.com>
2022-07-26 18:32:25 +0530

    Donate - Day2 - UI

--- app/src/main/java/com/example/alnoor/LoginActivity.java
+++ app/src/main/java/com/example/alnoor/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.alnoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,5 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void forgotPassword(View view) {
+
+
     }
 }
