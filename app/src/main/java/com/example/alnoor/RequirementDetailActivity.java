3d64d4e5b20daeb43e69a0bb31023a1ab84c7229
Roy <devedroy.dr@gmail.com>
2022-07-27 17:11:00 +0530

    MainActivity Navigation view init

---
+++ app/src/main/java/com/example/alnoor/RequirementDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.alnoor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequirementDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requirement_detail);
+    }
+}
---
+++ app/src/main/java/com/example/alnoor/RequirementListActivity.java
@@ -0,0 +1,14 @@
+package com.example.alnoor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequirementListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requirement_list);
+    }
+}
