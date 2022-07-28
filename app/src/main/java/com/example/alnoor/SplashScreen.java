8d81c407760452bd8f33775910dd427ac3ea3279
Roy <devedroy.dr@gmail.com>
2022-07-28 16:45:08 +0530

    SplashScreen init

--- app/src/main/java/com/example/alnoor/SplashScreen.java
+++ app/src/main/java/com/example/alnoor/SplashScreen.java
@@ -1,21 +1,43 @@
 package com.example.alnoor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startActivity(new Intent(SplashScreen.this, LogSignSelectorActivity.class));
-    }
+//    userSession =new
+//
+//    UserSession(SplashScreen .this);
+//
+//    Handler handler = new Handler();
+//        handler.postDelayed(new
+//
+//    Runnable() {
+//        @Override
+//        public void run () {
+//            if (userSession.isUserLoggedIn()) {
+//                Intent i11 = new Intent(SplashScreen.this, MainActivity.class);
+//                startActivity(i11);
+//                finish();
+//            } else {
+//                Intent i12 = new Intent(SplashScreen.this, Login_Activity.class);
+//                startActivity(i12);
+//                finish();
+//            }
+//        }
+//    },3000);
 }
