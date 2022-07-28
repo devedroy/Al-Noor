fb8a8611a6c86f8b86d4412f0648302aa3defd7e
Roy <devedroy.dr@gmail.com>
2022-07-28 16:01:43 +0530

    Base UI done

---
+++ app/src/main/java/com/example/alnoor/MyActivities.java
@@ -0,0 +1,14 @@
+package com.example.alnoor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_activities);
+    }
+}
