f770028454c78f36a3e86b8f0566a559f0874f1d
Roy <devedroy.dr@gmail.com>
2022-08-02 12:53:07 +0530

    NotificationsActivity UI init

--- app/src/main/java/com/example/alnoor/NotificationsActivity.java
+++ app/src/main/java/com/example/alnoor/NotificationsActivity.java
@@ -1,8 +1,9 @@
 package com.example.alnoor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+    }
+
+    public void backNotifications(View view) {
+        onBackPressed();
     }
 }
